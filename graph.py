fb69d453ef2f3fff059034c8b37065d72ac80bec
dracos-l <71078724+dracos-l@users.noreply.github.com>
2021-11-13 11:04:07 -0500

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,51 @@
+# Add a vertex to the dictionary
+graph = {}
+vertices_no = 0
+def add_vertex(v):
+  global graph
+  global vertices_no
+  if v in graph:
+    print("Vertex ", v, " already exists.")
+  else:
+    vertices_no = vertices_no + 1
+    graph[v] = []
+
+# Add an edge between vertex v1 and v2 with edge weight e
+def add_edge(v1, v2, e):
+  global graph
+  # Check if vertex v1 is a valid vertex
+  if v1 not in graph:
+    print("Vertex ", v1, " does not exist.")
+  # Check if vertex v2 is a valid vertex
+  elif v2 not in graph:
+    print("Vertex ", v2, " does not exist.")
+  else:
+    # Since this code is not restricted to a directed or 
+    # an undirected graph, an edge between v1 v2 does not
+    # imply that an edge exists between v2 and v1
+    temp = [v2, e]
+    graph[v1].append(temp)
+
+# Print the graph
+def print_graph():
+  global graph
+  for vertex in graph:
+    for edges in graph[vertex]:
+      print(vertex, " -> ", edges[0], " edge weight: ", edges[1])
+
+# driver code
+# stores the number of vertices in the graph
+for i in range(0,19):
+    add_vertex('t'+str(i))
+
+
+
+
+# Add the edges between the vertices by specifying
+# the from and to vertex along with the edge weights.
+add_edge('t1', 't2', 1)
+
+print_graph()
+# Reminder: the second element of each list inside the dictionary
+# denotes the edge weight.
+print ("Internal representation: ", graph)
